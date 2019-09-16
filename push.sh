26ad66caf5ab28443a246c50f6b50bcb91be5dd5
Adam Crow <adamcrow63@gmail.com>
2019-09-17 08:08:08 +1000

    Added push

---
+++ push.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+project=media-proxy
+file="src/main/resources/media-proxy-git.properties"
+
+function prop {
+    grep "${1}=" ${file}|cut -d'=' -f2
+}
+
+if [ -z "${1}" ]; then
+   version="latest"
+else
+   version="${1}"
+fi
+
+if [ -f "$file" ]
+then
+  echo "$file found."
+
+  echo "git.commit.id = " $(prop 'git.commit.id')
+  echo "git.build.version = " $(prop 'git.build.version')
+  docker push gennyproject/${project}:latest
+  docker push gennyproject/${project}:"${version}"
+  docker push gennyproject/${project}:$(prop 'git.commit.id')
+  docker push gennyproject/${project}:$(prop 'git.build.version')
+else
+  echo "ERROR: git properties $file not found."
+fi
+
