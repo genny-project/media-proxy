34746e663ecef9fdd6e0a92f12b2df32002220da
Jack Li <hbjoylee@gmail.com>
2019-11-12 11:53:41 +1100

    Update build and push docker images scripts

--- build-docker.sh
+++ build-docker.sh
@@ -3,27 +3,30 @@
 project=media-proxy
 file="src/main/resources/${project}-git.properties"
 
-function prop {
-    grep "${1}=" ${file}|cut -d'=' -f2
+function prop() {
+  grep "${1}=" ${file} | cut -d'=' -f2
 }
 
 if [ -z "${1}" ]; then
-   version="latest"
+  version="latest"
 else
-   version="${1}"
+  version="${1}"
 fi
 
-
-if [ -f "$file" ]
-then
+if [ -f "$file" ]; then
   echo "$file found."
+  echo "git.commit.id = " "$(prop 'git.commit.id')"
+  echo "git.build.version = " "$(prop 'git.build.version')"
+  docker build -t gennyproject/${project}:"${version}" .
 
-  echo "git.commit.id = " $(prop 'git.commit.id')
-  echo "git.build.version = " $(prop 'git.build.version')
-  docker build  -t gennyproject/${project}:${version} .
-  docker tag gennyproject/${project}:${version} gennyproject/${project}:$(prop 'git.commit.id')
-  docker tag gennyproject/${project}:${version} gennyproject/${project}:$(prop 'git.build.version')
 else
   echo "ERROR: git properties $file not found."
 fi
 
+#clean up
+image_ids=$(docker images | grep ${project} | grep none)
+if [ "${image_ids:-0}" == 0 ]; then
+  echo 'Skip clean up'
+else
+  docker images | grep ${project} | grep none | awk '{print $3}' | xargs docker rmi
+fi
--- push.sh
+++ push.sh
@@ -2,27 +2,28 @@
 project=media-proxy
 file="src/main/resources/media-proxy-git.properties"
 
-function prop {
-    grep "${1}=" ${file}|cut -d'=' -f2
+function prop() {
+  grep "${1}=" ${file} | cut -d'=' -f2
 }
 
 if [ -z "${1}" ]; then
-   version="latest"
+  version="latest"
 else
-   version="${1}"
+  version="${1}"
 fi
 
-if [ -f "$file" ]
-then
+if [ -f "$file" ]; then
   echo "$file found."
 
-  echo "git.commit.id = " $(prop 'git.commit.id')
-  echo "git.build.version = " $(prop 'git.build.version')
-  docker push gennyproject/${project}:latest
+  echo "git.commit.id = " "$(prop 'git.commit.id')"
+  echo "git.build.version = " "$(prop 'git.build.version')"
   docker push gennyproject/${project}:"${version}"
-  docker push gennyproject/${project}:$(prop 'git.commit.id')
-  docker push gennyproject/${project}:$(prop 'git.build.version')
+
+  docker tag gennyproject/${project}:"${version}" gennyproject/${project}:latest
+  docker push gennyproject/${project}:latest
+
+  docker tag gennyproject/${project}:"${version}" gennyproject/${project}:"$(prop 'git.build.version')"
+  docker push gennyproject/${project}:"$(prop 'git.build.version')"
 else
   echo "ERROR: git properties $file not found."
 fi
-
