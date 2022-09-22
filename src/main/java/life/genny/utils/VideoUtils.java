96a0e45fbd15ad6238921d7b17d2e47ac4810dac
mnzit <mnzitshakya@gmail.com>
2022-09-22 23:59:42 +0545

    removed changes

--- src/main/java/life/genny/utils/VideoUtils.java
+++ src/main/java/life/genny/utils/VideoUtils.java
@@ -1,6 +1,5 @@
 package life.genny.utils;
 
-import life.genny.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ws.schild.jave.Encoder;
@@ -10,7 +9,6 @@ import ws.schild.jave.encode.AudioAttributes;
 import ws.schild.jave.encode.EncodingAttributes;
 import ws.schild.jave.encode.VideoAttributes;
 import ws.schild.jave.encode.enums.X264_PROFILE;
-import ws.schild.jave.filters.ScaleFilter;
 import ws.schild.jave.info.MultimediaInfo;
 import ws.schild.jave.progress.EncoderProgressListener;
 
@@ -18,17 +16,14 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.UUID;
 
 public class VideoUtils {
     private static Logger log = LoggerFactory.getLogger(VideoUtils.class);
 
     public static File convert(String fileName, File input, String videoType, Integer videoBitrate) throws IOException, EncoderException {
         log.debug("#### Starting video conversion");
-        log.debug("#### This is the new code base");
         Instant start = Instant.now();
         File target = TemporaryFileStore.createTemporaryFile(fileName);
-
         AudioAttributes audio = new AudioAttributes();
         audio.setCodec("aac");
         audio.setBitRate(128000);
