0dd6f9ab90abd7fe24cfb6beebc83e6ae499c619
mnzit <mnzitshakya@gmail.com>
2022-09-22 23:24:39 +0545

    PIM-331 format changes

--- src/main/java/life/genny/utils/VideoUtils.java
+++ src/main/java/life/genny/utils/VideoUtils.java
@@ -34,10 +34,11 @@ public class VideoUtils {
         audio.setSamplingRate(44100);
         audio.setChannels(2);
         VideoAttributes video = new VideoAttributes();
-//        video.setCodec("h264");
-//        video.setX264Profile(X264_PROFILE.MAIN);
+        video.setCodec("h264");
+        video.setX264Profile(X264_PROFILE.BASELINE);
         video.setBitRate(videoBitrate);
 //        video.setFrameRate(30);
+        video.setPixelFormat("yuv420p");
         video.setFaststart(true);
         EncodingAttributes attrs = new EncodingAttributes();
 
